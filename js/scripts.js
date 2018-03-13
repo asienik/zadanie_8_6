118f67945ded6da05d670c0881f2f27a80674347
Joanna Fengler <asiapobuta@wp.pl>
2018-03-13 20:21:10 +0100

    task what if_js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Hello.
+//
+// This is a task "what if".
+
+/*alert var name = prompt('Enter your name');
+('Hello, ' + name);
+console.log('Hello, ' + name);*/
+var a = 2;
+var b = 3;
+var value = (a * a) - (2 * a * b) - (b * b);
+(value);
+console.log(value);
+if (value < 0) {
+   console.log('wynik ujemny');
+} else if (value > 0) {
+    console.log('wynik dodatni');
+} else {
+    console.log('wynik równy zero');
+}
